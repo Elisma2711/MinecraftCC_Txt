baa222c5ffcddd5919a6835292ffc732d69a7acc
Elisma2711 <ismael_aloy@hotmail.com>
2021-11-17 22:18:07 -0300

    v1.0.0 - Primera versión para testeos

--- MinecraftCC_Txt/Form1.Designer.cs
+++ MinecraftCC_Txt/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,241 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Btn_Refresh = new System.Windows.Forms.Button();
+            this.Btn_Modificar = new System.Windows.Forms.Button();
+            this.Btn_Eliminar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Text_Z = new System.Windows.Forms.TextBox();
+            this.Text_Y = new System.Windows.Forms.TextBox();
+            this.Text_X = new System.Windows.Forms.TextBox();
+            this.Text_Nombre = new System.Windows.Forms.TextBox();
+            this.Dgv_Coordenadas = new System.Windows.Forms.DataGridView();
+            this.Btn_Agregar = new System.Windows.Forms.Button();
+            this.Lbl_Mod = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Coordenadas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Refresh
+            // 
+            this.Btn_Refresh.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_Refresh.BackgroundImage = global::MinecraftCC_Txt.Properties.Resources.refresh;
+            this.Btn_Refresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Btn_Refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Refresh.Font = new System.Drawing.Font("Segoe UI Black", 12.5F, System.Drawing.FontStyle.Bold);
+            this.Btn_Refresh.Location = new System.Drawing.Point(438, 2);
+            this.Btn_Refresh.Name = "Btn_Refresh";
+            this.Btn_Refresh.Size = new System.Drawing.Size(40, 35);
+            this.Btn_Refresh.TabIndex = 23;
+            this.Btn_Refresh.UseVisualStyleBackColor = false;
+            this.Btn_Refresh.Click += new System.EventHandler(this.Btn_Refresh_Click);
+            // 
+            // Btn_Modificar
+            // 
+            this.Btn_Modificar.BackColor = System.Drawing.Color.Yellow;
+            this.Btn_Modificar.Enabled = false;
+            this.Btn_Modificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Modificar.Font = new System.Drawing.Font("Segoe UI Black", 12.5F, System.Drawing.FontStyle.Bold);
+            this.Btn_Modificar.Location = new System.Drawing.Point(174, 99);
+            this.Btn_Modificar.Name = "Btn_Modificar";
+            this.Btn_Modificar.Size = new System.Drawing.Size(141, 33);
+            this.Btn_Modificar.TabIndex = 17;
+            this.Btn_Modificar.Text = "MODIFICAR";
+            this.Btn_Modificar.UseVisualStyleBackColor = false;
+            this.Btn_Modificar.Click += new System.EventHandler(this.Btn_Modificar_Click);
+            // 
+            // Btn_Eliminar
+            // 
+            this.Btn_Eliminar.BackColor = System.Drawing.Color.Red;
+            this.Btn_Eliminar.Enabled = false;
+            this.Btn_Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Eliminar.Font = new System.Drawing.Font("Segoe UI Black", 12.5F, System.Drawing.FontStyle.Bold);
+            this.Btn_Eliminar.Location = new System.Drawing.Point(337, 99);
+            this.Btn_Eliminar.Name = "Btn_Eliminar";
+            this.Btn_Eliminar.Size = new System.Drawing.Size(141, 33);
+            this.Btn_Eliminar.TabIndex = 19;
+            this.Btn_Eliminar.Text = "ELIMINAR";
+            this.Btn_Eliminar.UseVisualStyleBackColor = false;
+            this.Btn_Eliminar.Click += new System.EventHandler(this.Btn_Eliminar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(421, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 21);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(316, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 21);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(211, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 21);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(42, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 21);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "NOMBRE";
+            // 
+            // Text_Z
+            // 
+            this.Text_Z.BackColor = System.Drawing.Color.White;
+            this.Text_Z.Location = new System.Drawing.Point(384, 64);
+            this.Text_Z.MaxLength = 10;
+            this.Text_Z.Name = "Text_Z";
+            this.Text_Z.Size = new System.Drawing.Size(93, 29);
+            this.Text_Z.TabIndex = 15;
+            this.Text_Z.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Text_KeyPress);
+            // 
+            // Text_Y
+            // 
+            this.Text_Y.BackColor = System.Drawing.Color.White;
+            this.Text_Y.Location = new System.Drawing.Point(279, 64);
+            this.Text_Y.MaxLength = 10;
+            this.Text_Y.Name = "Text_Y";
+            this.Text_Y.Size = new System.Drawing.Size(93, 29);
+            this.Text_Y.TabIndex = 14;
+            this.Text_Y.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Text_KeyPress);
+            // 
+            // Text_X
+            // 
+            this.Text_X.BackColor = System.Drawing.Color.White;
+            this.Text_X.Location = new System.Drawing.Point(174, 64);
+            this.Text_X.MaxLength = 10;
+            this.Text_X.Name = "Text_X";
+            this.Text_X.Size = new System.Drawing.Size(93, 29);
+            this.Text_X.TabIndex = 13;
+            this.Text_X.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Text_KeyPress);
+            // 
+            // Text_Nombre
+            // 
+            this.Text_Nombre.BackColor = System.Drawing.Color.White;
+            this.Text_Nombre.Location = new System.Drawing.Point(11, 64);
+            this.Text_Nombre.MaxLength = 100;
+            this.Text_Nombre.Name = "Text_Nombre";
+            this.Text_Nombre.Size = new System.Drawing.Size(141, 29);
+            this.Text_Nombre.TabIndex = 11;
+            // 
+            // Dgv_Coordenadas
+            // 
+            this.Dgv_Coordenadas.AllowUserToAddRows = false;
+            this.Dgv_Coordenadas.AllowUserToDeleteRows = false;
+            this.Dgv_Coordenadas.AllowUserToResizeRows = false;
+            this.Dgv_Coordenadas.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.Dgv_Coordenadas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Coordenadas.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Dgv_Coordenadas.Location = new System.Drawing.Point(0, 145);
+            this.Dgv_Coordenadas.MultiSelect = false;
+            this.Dgv_Coordenadas.Name = "Dgv_Coordenadas";
+            this.Dgv_Coordenadas.ReadOnly = true;
+            this.Dgv_Coordenadas.RowHeadersVisible = false;
+            this.Dgv_Coordenadas.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Dgv_Coordenadas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_Coordenadas.Size = new System.Drawing.Size(490, 175);
+            this.Dgv_Coordenadas.TabIndex = 12;
+            this.Dgv_Coordenadas.TabStop = false;
+            this.Dgv_Coordenadas.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Dgv_Coordenadas_CellMouseClick);
+            // 
+            // Btn_Agregar
+            // 
+            this.Btn_Agregar.BackColor = System.Drawing.Color.Green;
+            this.Btn_Agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Agregar.Font = new System.Drawing.Font("Segoe UI Black", 12.5F, System.Drawing.FontStyle.Bold);
+            this.Btn_Agregar.Location = new System.Drawing.Point(11, 99);
+            this.Btn_Agregar.Name = "Btn_Agregar";
+            this.Btn_Agregar.Size = new System.Drawing.Size(141, 33);
+            this.Btn_Agregar.TabIndex = 16;
+            this.Btn_Agregar.Text = "AGREGAR";
+            this.Btn_Agregar.UseVisualStyleBackColor = false;
+            this.Btn_Agregar.Click += new System.EventHandler(this.Btn_Agregar_Click);
+            // 
+            // Lbl_Mod
+            // 
+            this.Lbl_Mod.AutoSize = true;
+            this.Lbl_Mod.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Mod.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Lbl_Mod.ForeColor = System.Drawing.Color.DimGray;
+            this.Lbl_Mod.Location = new System.Drawing.Point(7, 12);
+            this.Lbl_Mod.Name = "Lbl_Mod";
+            this.Lbl_Mod.Size = new System.Drawing.Size(425, 19);
+            this.Lbl_Mod.TabIndex = 24;
+            this.Lbl_Mod.Text = "¤ Para modificar una coordenada reescríbla y haga click en modificar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(490, 320);
+            this.Controls.Add(this.Lbl_Mod);
+            this.Controls.Add(this.Btn_Refresh);
+            this.Controls.Add(this.Btn_Modificar);
+            this.Controls.Add(this.Btn_Eliminar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Text_Z);
+            this.Controls.Add(this.Text_Y);
+            this.Controls.Add(this.Text_X);
+            this.Controls.Add(this.Text_Nombre);
+            this.Controls.Add(this.Dgv_Coordenadas);
+            this.Controls.Add(this.Btn_Agregar);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "INGRESAR COORDENADAS";
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Coordenadas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_Refresh;
+        private System.Windows.Forms.Button Btn_Modificar;
+        private System.Windows.Forms.Button Btn_Eliminar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Text_Z;
+        private System.Windows.Forms.TextBox Text_Y;
+        private System.Windows.Forms.TextBox Text_X;
+        private System.Windows.Forms.TextBox Text_Nombre;
+        private System.Windows.Forms.DataGridView Dgv_Coordenadas;
+        private System.Windows.Forms.Button Btn_Agregar;
+        private System.Windows.Forms.Label Lbl_Mod;
     }
 }
-
--- MinecraftCC_Txt/Form1.cs
+++ MinecraftCC_Txt/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,183 @@ namespace MinecraftCC_Txt
         public Form1()
         {
             InitializeComponent();
+
+            LlenarDgv();
+            Botones(false);
+        }
+
+        private void Btn_Refresh_Click(object sender, EventArgs e)
+        {
+            LlenarDgv();
+            Botones(false);
+            Text_Nombre.Text = "";
+            Text_X.Text = "";
+            Text_Y.Text = "";
+            Text_Z.Text = "";
+        }
+
+        private void Btn_Agregar_Click(object sender, EventArgs e)
+        {
+            // Agreagar una coordenada
+            // https://www.c-sharpcorner.com/article/c-sharp-write-to-file/
+            // if (File.Exists(textFile)) -- Poner en todos lados
+            if (!String.IsNullOrEmpty(Text_Nombre.Text) && !String.IsNullOrEmpty(Text_X.Text) && !String.IsNullOrEmpty(Text_Y.Text) && !String.IsNullOrEmpty(Text_Z.Text))
+            {
+                string[] coordenadas = File.ReadAllLines("Coordenadas.txt");
+                string newCC = Text_Nombre.Text + "|" + Text_X.Text + "|" + Text_Y.Text + "|" + Text_Z.Text;
+
+                string[] newCoorArr = new string[coordenadas.Length + 1];
+                for(int i = 0; i < newCoorArr.Length; i++)
+                {
+                    if (i < coordenadas.Length)
+                    {
+                        newCoorArr[i] = coordenadas[i];
+                    }
+                    else
+                    {
+                        newCoorArr[i] = newCC;
+                    }
+                }
+
+                File.WriteAllLines("Coordenadas.txt", newCoorArr);
+                LlenarDgv();
+            }
+            else
+            {
+                MessageBox.Show("Debes llenar todos los campos");
+            }
+        }
+
+        private void Btn_Modificar_Click(object sender, EventArgs e)
+        {
+            // Modificar una coordenada
+            // Seria algo como eliminarla y volver a crearla o algo asi
+            var coordenadas = File.ReadAllLines("Coordenadas.txt");
+            var nuevasCC = coordenadas.Where(line => !line.Contains(Dgv_Coordenadas.CurrentRow.Cells[0].Value.ToString() + "|"));
+            File.WriteAllLines("Coordenadas.txt", nuevasCC);
+
+            if (!String.IsNullOrEmpty(Text_Nombre.Text) && !String.IsNullOrEmpty(Text_X.Text) && !String.IsNullOrEmpty(Text_Y.Text) && !String.IsNullOrEmpty(Text_Z.Text))
+            {
+                string[] coordenadasa = File.ReadAllLines("Coordenadas.txt");
+                string newCC = Text_Nombre.Text + "|" + Text_X.Text + "|" + Text_Y.Text + "|" + Text_Z.Text;
+
+                string[] newCoorArr = new string[coordenadasa.Length + 1];
+                for (int i = 0; i < newCoorArr.Length; i++)
+                {
+                    if (i < coordenadasa.Length)
+                    {
+                        newCoorArr[i] = coordenadasa[i];
+                    }
+                    else
+                    {
+                        newCoorArr[i] = newCC;
+                    }
+                }
+
+                File.WriteAllLines("Coordenadas.txt", newCoorArr);
+                LlenarDgv();
+            }
+            else
+            {
+                MessageBox.Show("Debes llenar todos los campos");
+            }
+        }
+
+        private void Btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            // Eliminar una coordenada
+            // https://www.c-sharpcorner.com/blogs/read-write-and-delete-the-text-file-content-using-c-sharp
+            // if (File.Exists(textFile))
+            var coordenadas = File.ReadAllLines("Coordenadas.txt");
+            var nuevasCC = coordenadas.Where(line => !line.Contains(Dgv_Coordenadas.CurrentRow.Cells[0].Value.ToString() + "|"));
+            File.WriteAllLines("Coordenadas.txt", nuevasCC);
+            LlenarDgv();
+        }
+
+        private void Text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar != (char)Keys.Back) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.') && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                // Solo se pueden poner numeros
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.'))
+            {
+                e.KeyChar = ',';
+            }
+
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                // No se puede poner mas de un -
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
+            {
+                // No se puede poner mas de un -
+                e.Handled = true;
+            }
+        }
+
+        private void Dgv_Coordenadas_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Botones(true);
+            // Escribirlo en los TextBox
+            Text_Nombre.Text = Dgv_Coordenadas.CurrentRow.Cells[0].Value.ToString();
+            Text_X.Text = Dgv_Coordenadas.CurrentRow.Cells[1].Value.ToString();
+            Text_Y.Text = Dgv_Coordenadas.CurrentRow.Cells[2].Value.ToString();
+            Text_Z.Text = Dgv_Coordenadas.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void LlenarDgv()
+        {
+            // Leer el txt y separar los strings por |
+            Dgv_Coordenadas.DataSource = null;
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("Nombre");
+            dataTable.Columns.Add("X");
+            dataTable.Columns.Add("Y");
+            dataTable.Columns.Add("Z");
+
+            string[] lineas = File.ReadAllLines("Coordenadas.txt");
+            
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string line = lineas[i];
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] splitted = line.Split('|');
+                    dataTable.Rows.Add(splitted[0], splitted[1], splitted[2], splitted[3]);
+                    Dgv_Coordenadas.DataSource = dataTable;
+
+                    Dgv_Coordenadas.Columns[0].Width = Dgv_Coordenadas.Width / 2;
+                    Dgv_Coordenadas.Columns[1].Width = Dgv_Coordenadas.Width / 6;
+                    Dgv_Coordenadas.Columns[2].Width = Dgv_Coordenadas.Width / 6;
+                    Dgv_Coordenadas.Columns[3].Width = Dgv_Coordenadas.Width / 6;
+                } 
+            }
+        }
+
+        private void Botones(bool a)
+        {
+            // Si haces click en una celda del dgv se activan los btns y sino no xd
+            if (a)
+            {
+                Btn_Modificar.Enabled = true;
+                Btn_Eliminar.Enabled = true;
+                Btn_Modificar.BackColor = Color.Yellow;
+                Btn_Eliminar.BackColor = Color.Red;
+            }
+            else
+            {
+                Btn_Modificar.Enabled = false;
+                Btn_Eliminar.Enabled = false;
+                Btn_Modificar.BackColor = Color.Beige;
+                Btn_Eliminar.BackColor = Color.LightCoral;
+                Dgv_Coordenadas.ClearSelection();
+            }
         }
     }
 }
